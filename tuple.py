ace2163d3c1270535174a66e8d3d15ea2fa8ba24
JDamianx <djandroidgt@gmail.com>
2025-10-28 19:30:03 +0100

    Add tuple type quick exercises.

---
+++ tuple.py
@@ -0,0 +1,38 @@
+# tuple_1 = (1, 2, 3)
+# for elem in tuple_1:
+#     print(elem)
+ 
+# # Example 2
+# tuple_2 = (1, 2, 3, 4)
+# print(5 in tuple_2)
+# print(5 not in tuple_2)
+ 
+# # Example 3
+# tuple_3 = (1, 2, 3, 4)
+# print(len(tuple_3))
+# print(5 not in tuple_3)
+# # Example 4
+# tuple_4 = tuple_1 + tuple_2
+# tuple_5 = tuple_3 * 2
+ 
+# print(tuple_4)
+# print(tuple_5)
+# #  ******************************************************************************************
+# d1 = {'Adam Smith': 'A', 'Judy Paxton': 'B+'}
+# d2 = {'Mary Louis': 'A', 'Patrick White': 'C'}
+# d3 = {}
+ 
+# for item in (d1, d2):
+#     d3.update(item)
+ 
+# print(d3)
+#  ******************************************************************************************
+# my_list = ["car", "Ford", "flower", "Tulip"]
+ 
+# t = tuple(my_list)
+# print(t)
+#  ******************************************************************************************
+colors = (("green", "#008000"), ("blue", "#0000FF"))
+colors_dictionary={color: id for color, id in colors}
+ 
+print(colors_dictionary)
